f1015947ed8cbb24ef21b84e662479da37fecc48
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-05 15:56:22 +0300

    Code: Angular services added
    http service added and the scope well defined in an array to avoid conflicts. http service allows us to read from external files(data.json) and get the data from there

--- js/controllers.js
+++ js/controllers.js
@@ -1,47 +1,9 @@
 var App = angular.module("myApp", []);
 
-App.controller('AuthorController', function AuthorController($scope){
-	$scope.persons = [
-	{
-		 "name" : "Denis Karanja",
-		 "title" : "Developer",
-		 "company" : "Deebeat City KE",
-		 "bio" : "The lead developer for Deebeat City KE"
-	},
+App.controller('AuthorController', ['$scope', '$http', function AuthorController($scope, $http){
 
-	{
-		 "name" : "Moses James",
-		 "title" : "Designer",
-		 "company" : "Yahoo Inc",
-		 "bio" : "The lead designer in Yahoo Inc"
-	},
+		$http.get("js/data.json").success(function(data){
+			$scope.persons = data; 
+		});
 
-	{
-		 "name" : "Jessica Lord",
-		 "title" : "Analyst",
-		 "company" : "Github",
-		 "bio" : "Business analyst at Github"
-	},
-
-	{
-		 "name" : "Maureen Karanja",
-		 "title" : "Strategist",
-		 "company" : "Deebeat City KE",
-		 "bio" : "The lead strategist in the company"
-	},
-
-	{
-		 "name" : "Rosemary Muchiri",
-		 "title" : "Medical Officer",
-		 "company" : "Deebeat City KE",
-		 "bio" : "The lead MO in the company"
-	},
-
-	{
-		 "name" : "Fridah Karanja",
-		 "title" : "Human Resource",
-		 "company" : "Barclays Bank KE",
-		 "bio" : "Human Resource assistant"
-	}
-]
-});
+}]);
