45b7bf7670ac5ef7215887b25f7bc2061dea50f5
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-05 16:27:06 +0300

    Code: More filters
    Filters using select groups and radio buttons to bind to data on the page to enhance search functionalitites

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,10 @@ var App = angular.module("myApp", []);
 App.controller('AuthorController', ['$scope', '$http', function AuthorController($scope, $http){
 
 		$http.get("js/data.json").success(function(data){
-			$scope.persons = data; 
+			$scope.persons = data;
+
+			/*pre-initialize the select group*/
+			$scope.personsOrder = 'name'; 
 		});
 
 }]);
